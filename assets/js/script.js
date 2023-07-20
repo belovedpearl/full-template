d112ac4678b4f52af76d08bf4840730429717f9e
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-07-20 11:12:45 +0000

    Display result with the modal and modify the options

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,60 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e))
 
+// Check that values are right as required by API
+
+function processData(form){
+    let optArray = [];
+
+    // Check through the form using the entries()
+    for (let entry of form.entries()){
+        if (entry[0] === "options"){
+            optArray.push(entry[1])
+        }
+    }
+
+    // form data method of delete
+    form.delete("options"); // delete all occurence of option
+    form.append("options", optArray.join())
+
+    return form
+}
+
 // Post request
 async function postForm(e){
-    const form = new FormData(document.getElementById("checksform"))
-   
+    const form = processData(new FormData(document.getElementById("checksform")));
+
     const response = await fetch(API_URL, {
                         method: "POST",
                         headers: {
                                     "Authorization": API_KEY,
                                  },
                         body: form,
-                        })
-            
+                        });
+    const data = await response.json();
+    if (response.ok){
+        displayError(data)
+    }else{
+        throw new Error(data.error);
+    }
+}
+
+// Display error function
+function displayError(data) {
+    let heading =  `JS hint Results for ${data.file}`;
+    if (data.total_errors === 0){
+        results = `<div class="no-errors">No errors to report!</div>`
+    } else {
+        results =  `<div>Total Errors: <span class="error-count">${data.total_errors}</span>`;
+        for (let error of data.error_list){
+            results += `<div>At line <span class="line">${error.line}</span>,`
+            results += `column <span class="column">${error.col}<\span>`;
+            results += `<div class="error">${error.error}</div>`
+        }
+        document.getElementById("resultsModalTitle").innerText = heading;
+        document.getElementById("results-content").innerHTML = results;
+        resultsModal.show()
+    }
 }
 
 // Get request
